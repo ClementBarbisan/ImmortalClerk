da4ffd9481147f6884bfb5f28d2f2e38ec848292
randomLockNess <c.barbisan@free.fr>
2020-11-10 11:01:51 +0100

    initial commit

---
+++ Assets/Scipts/JsonParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class JsonParser : MonoBehaviour
+{
+    public static JsonParser Instance;
+    [Serializable]
+    public struct Data
+    {
+        public List<Technos> TechnologyTree;
+        public List<Descends> Descendants;
+        public List<Auto> AutoTech;
+    };
+
+    [Serializable]
+    public struct WordsStruct
+    {
+        public List<string> subjects;
+        public List<string> verbs;
+        public List<string> objects;
+    };
+
+    [Serializable]
+    public struct Technos
+    {
+        public string name;
+        public List<string> dependances;
+        public WordsStruct words;
+        public bool useable;
+    };
+
+    [Serializable]
+    public struct Descends
+    {
+        public string name;
+        public Vector4 limits;
+        public Vector4 value;
+        public bool useable;
+    };
+
+    [Serializable]
+    public struct Auto
+    {
+        public string name;
+        public string dependance;
+        public Vector4 limits;
+        public float time;
+        public bool useable;
+    };
+    public Data data;
+    [FormerlySerializedAs("nameFile")] [SerializeField] private string _nameFile;
+    private void Start()
+    {
+        Instance = this;
+        data = JsonUtility.FromJson<Data>(File.ReadAllText(Application.streamingAssetsPath + "/" + _nameFile));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scipts/ScribeManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScribeManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _listContent;
+    [SerializeField] private Slider _religion;
+    [SerializeField] private Slider _science;
+    [SerializeField] private Slider _social;
+    [SerializeField] private Slider _conquest;
+    [SerializeField] private GameObject _prefab;
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
